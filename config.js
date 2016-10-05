0dc5b713d85c03889cfd832ff31fc3b4ed0f06f5
Mathew Varughese <mat@imginconline.com>
2016-10-05 19:04:41 -0400

    css stuff

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 8080,
-    database: 'mongodb://varughese:Blackjack21@ds011745.mlab.com:11745/hhnhs',
+    database: 'mongodb://varughese:Blackjack21@ds049446.mlab.com:49446/hhkeyclub',
     secret: 'jawnjawnjawnjawn'
 };
